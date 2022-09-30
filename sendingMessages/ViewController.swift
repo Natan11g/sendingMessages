6fa92f0c90054d9596fef1f29011f6f7c8ab5927
Natan <natan.gluszko@natwest.com>
2022-09-30 13:02:47 +0100

    messages and emails

--- sendingMessages/ViewController.swift
+++ sendingMessages/ViewController.swift
@@ -6,14 +6,62 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMailComposeViewControllerDelegate, MFMessageComposeViewControllerDelegate{
+    
 
+    let messageViewController = MFMailComposeViewController()
+    let smsViewController = MFMessageComposeViewController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        messageViewController.mailComposeDelegate = self
+        smsViewController.messageComposeDelegate = self
+        
     }
 
+    @IBAction func btnCompose(_ sender: UIButton) {
+        
+        if !MFMailComposeViewController.canSendMail() {
+            print("mail service are not available")
+        }
+        else {
+            messageViewController.setToRecipients(["address@example.com"])
+            messageViewController.setSubject("Hello!")
+            messageViewController.setMessageBody("Hi it's me natan!", isHTML: false)
+            self.present(messageViewController, animated: true, completion: nil)
+        }
+    }
+    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func smsBtn(_ sender: Any) {
+        
+        if !MFMessageComposeViewController.canSendText() {
+            print("SMS service are not available")
+        }
+        else {
+            smsViewController.recipients = ["4085551212"]
+            smsViewController.body = "This is a message"
+            present(smsViewController, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+     dismiss(animated: true, completion: nil)
+     }
 
+    
+    
+    
+    
 }
 
